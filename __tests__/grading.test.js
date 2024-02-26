745d2363271d0fc642a51de51d6c22d897245a87
wisevaishu <vpm.win@gmail.com>
2024-02-25 21:57:42 -0600

    Graded Assignment 4 First Commit

--- __tests__/grading.test.js
+++ __tests__/grading.test.js
@@ -32,13 +32,14 @@ describe ("Grading Tests: ", function () {
 
    it("HTML contains correct number of sections", function() {
       let childrenElements = document.body.children;
-
+      
       expect(childrenElements.length).toBeGreaterThanOrEqual(3);
       expect(childrenElements.length).toBeLessThanOrEqual(10);
    })
 
    it("HTML includes external CSS script", function() {
       let linkElement = document.getElementsByTagName("Link");
+      
       expect(linkElement.item(0).href.includes('styles.css')).toBe(true);
    })
 
